8af638a751bdc464629dd4b565f341124a0bf82c
Raja Singh <rajasgh18@gmail.com>
2024-04-02 04:13:04 +0530

    Added filter and search route to the main server file

--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
+// Importing Packges/Functions
 const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
 const CORS = require('cors');
 const connectToDB = require('./db.js');
 
-const userRoute = require('./routes/user.js');
+// Importing Routes
+const userRoute = require('./routes/user');
+const filterRoute = require('./routes/user/filter.js');
+const searchRoute = require('./routes/user/search.js');
 
-
+// Applying CORS and added parsing JSON so that JSON data becomes available in req.body
 app.use(CORS());
 app.use(express.json());
-app.use(bodyParser.json());
 
+// Connecting to MongoDB using Mongoose
 connectToDB();
 
+// Defining routes for the API
 app.get('/', (req, res)=> res.send("This is an Server for Heliverse Assignment"));
-
+app.use("/api/users/search", searchRoute);
+app.use("/api/users/filter", filterRoute);
 app.use("/api/users", userRoute);
 
+// Running the Server at port 5000
 app.listen(5000, () => {
     console.log("Server listening at http://localhost:5000");
 });
