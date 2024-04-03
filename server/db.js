ebc12d215982b0dbb9af8c8cfa01afdd8a257df3
Raja Singh <rajasgh18@gmail.com>
2024-04-04 03:09:09 +0530

    Changing strict populate to false

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connetToDB = () => {
+    mongoose.set("strictPopulate", false);
     mongoose.connect(process.env.MONGO_DB_URL);
     const db = mongoose.connection;
     db.on('connected', () => console.log("MongoDB is connected"));
