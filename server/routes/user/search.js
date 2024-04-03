732ec959f516c44b28827aefb22a309f316b0a0c
Raja Singh <rajasgh18@gmail.com>
2024-04-03 06:20:55 +0530

    Added pagination to search

--- server/routes/user/search.js
+++ server/routes/user/search.js
@@ -4,11 +4,16 @@ const User = require('../../model/User');
 Router
     .get('/', async (req, res) => {
         const { first_name, last_name } = req.query;
-        if(!first_name || !last_name) return res.status(400).send("Please send a valid query");
-        try {
 
+        // Get the page number from the query and convert it to int if an string is passed it will send invalid page no.
+        const page = parseInt(req.query.page);
+        if (!page || page < 1) return res.status(400).send("Invalid Page number");
+        const pageSize = 20;
+        const skip = (page - 1) * pageSize;
+
+        try {
             // Constructing a syntax for searching parameters in which 
-            // $and means that if first_name and last_name exists at the same time
+            // $and means that if first_name and last_name exist at the same time
             // then it would return those users which contain both the searching params
             const search = {
                 $and: []
@@ -17,17 +22,24 @@ Router
             // $regex is used for partial matching of searching parameters
             if (first_name) search.$and.push({ first_name: { $regex: new RegExp(first_name, 'i') } });
             if (last_name) search.$and.push({ last_name: { $regex: new RegExp(last_name, 'i') } });
-            
-            const user = await User.find(search);
+
+            let users = await User.find(search).skip(skip).limit(pageSize);
+            let totalUsers = await User.countDocuments(search);
+            const totalPages = Math.ceil(totalUsers / pageSize);
+
+            if (page > totalPages) return res.status(400).send("Invalid Page number");
 
             res.status(200).json({
-                results: user.length,
-                user
+                currentPage: page,
+                totalPages,
+                pageSize,
+                users
             });
         } catch (error) {
             res.status(500).send("Error fetching users");
             console.log(error);
         }
     })
+
 
 module.exports = Router;
