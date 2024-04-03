c936729e2174926809c8ff48d7f0fa75f2864899
Raja Singh <rajasgh18@gmail.com>
2024-04-04 03:11:20 +0530

    adding all the reducers to strore

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,11 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../features/userSlice';
 import teamSlice from '../features/teamSlice';
+import sidebarSlice from '../features/sidebarSlice';
+import teambarSlice from '../features/teambarSlice';
+import teambarDataSlice from '../features/teambarDataSlice';
+import pageSlice from '../features/pageSlice';
 
 const store = configureStore({
     reducer: {
         user: userSlice,
-        team: teamSlice
+        team: teamSlice,
+        sidebar: sidebarSlice,
+        teambar: teambarSlice,
+        teambarData: teambarDataSlice,
+        page: pageSlice
     }
 });
 
