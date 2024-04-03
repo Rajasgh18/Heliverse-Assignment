501fc35797eb2d0e27052b5f0bcb502d37be26d0
Raja Singh <rajasgh18@gmail.com>
2024-04-04 03:11:36 +0530

    not found page

---
+++ client/src/pages/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <section className='h-screen w-full flex flex-col justify-center items-center gap-4'>
+            <h1 className="text-7xl">404</h1>
+            <p className="text-4xl">Page not found</p>
+        </section>
+    );
+};
+
+export default NotFound;
